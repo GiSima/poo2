cd0437b721fd425768adb7504be72a33d3148558
Pedro Trevisan <104599997+pedrootrevisan@users.noreply.github.com>
2024-04-23 21:40:51 -0300

    Add files via upload

---
+++ src/bin/freezemonsters/CommonsFreezeMonsters.java
@@ -0,0 +1,35 @@
+//
+// Source code recreated from a .class file by IntelliJ IDEA
+// (powered by FernFlower decompiler)
+//
+
+package bin.FreezeMonsters;
+
+import bin.spriteframework.Commons;
+
+public interface CommonsFreezeMonsters extends Commons {
+    int SLIME_HEIGHT = 30;
+    int SLIME_WIDTH = 30;
+    int SLIME_SPEED = 2;
+    int MONSTER_HEIGHT = 50;
+    int MONSTER_WIDTH = 50;
+    int NUMBER_OF_MONSTERS_TO_DESTROY = 24;
+    int PLAYER_WIDTH = 50;
+    int PLAYER_HEIGHT = 50;
+    int MONSTER_SPEED = 1;
+    int PLAYER_SPEED = 5;
+    int RAY_WIDTH = 30;
+    int RAY_HEIGHT = 30;
+    int RAY_SPEED = 5;
+    String pathMonster = "FreezeMonsters/images/monster";
+    String IMAGE_PLAYER = "FreezeMonsters/images/woody.png";
+    String IMAGE_SLIME = "FreezeMonsters/images/gosma.png";
+    String IMAGE_RAY = "FreezeMonsters/images/ray.png";
+    int VARIETY_MONSTERS = 9;
+    int BORDER_TOLERANCE = 5;
+    int INIT_PLAYER_X = 500;
+    int INIT_PLAYER_Y = 700;
+    int DELAY = 17;
+
+
+}
