6d90611d91a6852abf3a1db3de744630aaba44a7
Pedro Trevisan <104599997+pedrootrevisan@users.noreply.github.com>
2024-04-24 13:31:59 -0300

    commit spriteframework
    divisão abstractBoard gameCycle e TAdapter

--- src/bin/spriteframework/Commons.java
+++ src/bin/spriteframework/Commons.java
@@ -1,13 +1,12 @@
 package bin.spriteframework;
 
 public interface Commons {
-    int BOARD_WIDTH = 1400;
-    int BOARD_HEIGHT = 800;
-
-    int BORDER_RIGHT = BOARD_WIDTH - 5;
+    int BOARD_WIDTH = 358;
+    int BOARD_HEIGHT = 350;
+    int BORDER_RIGHT = 30;
     int BORDER_LEFT = 5;
     int PLAYER_SPEED = 5;
-    int INIT_PLAYER_X = 500;
-    int INIT_PLAYER_Y = 700;
+    int INIT_PLAYER_X = 270;
+    int INIT_PLAYER_Y = 280;
     int DELAY = 17;
 }
